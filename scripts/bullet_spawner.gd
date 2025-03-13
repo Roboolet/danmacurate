2228eef92e5638d5f64a7ded7685306744b51269
Lea <39883637+Roboolet@users.noreply.github.com>
2025-03-14 00:22:45 +0100

    Created bullet spawner

---
+++ scripts/bullet_spawner.gd
@@ -0,0 +1,30 @@
+extends Sprite2D
+class_name BulletSpawner
+
+@export var bullet_prefab:PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("layer_shoot"):
+		var test_data = BulletData.new()
+		test_data.offset.x = cos(Time.get_ticks_msec() * 0.01)*20
+		test_data.offset.y = sin(Time.get_ticks_msec() * 0.01)*20
+		spawn_bullet(test_data)
+	pass
+
+
+func spawn_bullet(data: BulletData) -> void:
+	var bullet:Node2D = bullet_prefab.instantiate()
+	add_child(bullet)
+	bullet.position.x = data.offset.x
+	bullet.position.y = data.offset.y
+	pass
+
+class BulletData:
+	var offset: Vector2
+	var offset_polar: Vector2
